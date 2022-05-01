17b36a9533664a0e755cffa30b7f2ccd9a6799d5
Tetsuki Syu <deadshot465@gmail.com>
2022-05-01 19:28:19 +0900

    Add endpoint.

--- src/controller/record_controller.rs
+++ src/controller/record_controller.rs
@@ -1,11 +1,12 @@
 use actix_web::{get, HttpResponse, post, Responder};
-use actix_web::web::{Data, ServiceConfig};
+use actix_web::web::{Data, Path, ServiceConfig};
 use sqlx::{Pool, Postgres};
 use crate::model::games::witchs_bar::PlayerRecordPayload;
 
 pub fn config_record_controller(cfg: &mut ServiceConfig) {
     cfg.service(get_all_witchs_bar_records)
-        .service(post_witchs_bar_record);
+        .service(post_witchs_bar_record)
+        .service(get_stage_witchs_bar_records);
 }
 
 #[get("/games/witchs_bar")]
@@ -28,7 +29,34 @@ async fn get_all_witchs_bar_records(data: Data<Pool<Postgres>>) -> impl Responde
             }
         }
         Err(e) => {
-            let error_msg = format!("An error occurred when querying from the database: {:?}", &e);
+            let error_msg = format!("An error occurred when querying the database: {:?}", &e);
+            log::error!("{}", &error_msg);
+            HttpResponse::InternalServerError().body(error_msg)
+        }
+    }
+}
+
+#[get("/games/witchs_bar/{stage_id}")]
+async fn get_stage_witchs_bar_records(stage_id: Path<i32>, data: Data<Pool<Postgres>>) -> impl Responder {
+    let query_result = sqlx::query_as::<_, PlayerRecordPayload>("SELECT * FROM \"WitchsBarRecords\" WHERE \"stage\" = $1")
+        .bind(&*stage_id)
+        .fetch_optional(&**data)
+        .await;
+
+    match query_result {
+        Ok(opt) => {
+            match opt {
+                None => {
+                    let error_msg = format!("Cannot find corresponding records for the specified level {}.", stage_id.into_inner());
+                    HttpResponse::NotFound().body(error_msg)
+                }
+                Some(payload) => {
+                    HttpResponse::Ok().json(payload)
+                }
+            }
+        }
+        Err(e) => {
+            let error_msg = format!("An error occurred when querying the database: {:?}", &e);
             log::error!("{}", &error_msg);
             HttpResponse::InternalServerError().body(error_msg)
         }
